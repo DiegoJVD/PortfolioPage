8c546945d2ed20422483782a13bcc727ec3ef2d7
Diego Valerio <Diego Valerio@siglo21.local>
2022-11-04 08:19:06 -0400

    Corriguiendo ruta de pm2 para servir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     script: "serve",
     name: "Portfolio", 
     env: {
-      PM2_SERVE_PATH: './public',
+      PM2_SERVE_PATH: './build',
       
       PM2_SERVE_PORT: 5001
     }
